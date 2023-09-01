3dca3fa7849a879d39564255f879a6f3c413ce30
Sheng-Wei Chen <will945945945@gmail.com>
2023-09-01 12:44:03 +0800

    add idx

--- bagging_linear.py
+++ bagging_linear.py
@@ -16,6 +16,7 @@ parser.add_argument('--beam_width', type=int, default=10000)
 parser.add_argument('--num_models', type=int, default=3)
 parser.add_argument('--sample_rate', type=float, default=0.1)
 parser.add_argument('--datapath', type=str, default="")
+parser.add_argument('--idx', type=int, default=-1)
 
 ARGS = parser.parse_args()
 
@@ -42,11 +43,11 @@ training_start = time.time()
 # model.bias /= num_models
 # 
 # preds = linear.predict_values(model, datasets["test"]["x"])
-def predict_in_batches(model_name, batch_size):
+def predict_in_batches(model_name, batch_size, model_idx):
     num_instances = datasets["test"]["x"].shape[0]
     num_batches = math.ceil(num_instances / batch_size)
 
-    for model_idx in range(ARGS.num_models):
+    if model_idx >= 0:
         submodel_name = "./models/" + model_name + "-{}".format(model_idx)
         sub_start = time.time()
         with open(submodel_name, "rb") as F:
@@ -71,6 +72,33 @@ def predict_in_batches(model_name, batch_size):
             with open(pred_name, "wb") as F:
                 pickle.dump((preds, indices), F)
             print("dump cost:", time.time()-sub_start, flush=True)
+
+    else:
+        for model_idx in range(ARGS.num_models):
+            submodel_name = "./models/" + model_name + "-{}".format(model_idx)
+            sub_start = time.time()
+            with open(submodel_name, "rb") as F:
+                tmp = pickle.load(F)
+            tmp, indices = tmp
+            print("{}th model loaded:".format(model_idx), time.time()-sub_start, flush=True)
+        
+            for i in range(num_batches):
+                pred_name = "./preds/" + model_name.split(".model")[0] + "-{}".format(model_idx) + "_batch-idx-{}".format(i) 
+                if os.path.isfile(pred_name):
+                    continue
+
+                sub_start = time.time()
+                print("process batches id:", i, flush=True)
+                start = time.time()
+                tmp_data = datasets["test"]["x"][i * batch_size : (i + 1) * batch_size]
+                preds = tmp.predict_values(tmp_data, beam_width=ARGS.beam_width)
+                print("sub cost:", time.time()-sub_start, flush=True)
+
+                print("dump {}th preds".format(model_idx), flush=True)
+                sub_start = time.time()
+                with open(pred_name, "wb") as F:
+                    pickle.dump((preds, indices), F)
+                print("dump cost:", time.time()-sub_start, flush=True)
 
 
 def metrics_in_batches(model_name, batch_size):
@@ -116,7 +144,7 @@ model_name = "Rand-label-Forest_{data}_seed={seed}_K={K}_sample-rate={sample_rat
 
 #metrics = metrics_in_batches(model_name, 10000)
 #metrics = metrics_in_batches(model_name, 1000)
-predict_in_batches(model_name, 10000)
+predict_in_batches(model_name, 10000, ARGS.idx)
 
 #print("mean in subsampled labels:", metrics)
 
