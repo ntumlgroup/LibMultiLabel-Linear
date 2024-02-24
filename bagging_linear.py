e85d268fab5d9b84d18dc89a2955344efe7aee1d
Sheng-Wei Chen <will945945945@gmail.com>
2024-02-24 02:37:31 +0000

    update

--- bagging_linear.py
+++ bagging_linear.py
@@ -68,7 +68,45 @@ def predict_in_batches(model_name, batch_size, model_idx):
         with open(pred_name, "wb") as F:
             pickle.dump((preds, indices), F)
         print("dump cost:", time.time()-sub_start, flush=True)
-        
+
+def metrics_in_batches_without_pred(model_name, batch_size):
+    num_instances = datasets["test"]["x"].shape[0]
+    num_batches = math.ceil(num_instances / batch_size)
+
+    metrics = linear.get_metrics(["P@1", "P@3", "P@5"], num_classes=datasets["test"]["y"].shape[1])
+    for i in range(num_batches):
+        print("process batches id:", i, flush=True)
+        start = time.time()
+        tmp_data = datasets["test"]["x"][i * batch_size : (i + 1) * batch_size]
+        total_preds = np.zeros([tmp_data.shape[0], datasets["train"]["y"].shape[1]], order='F')
+        total_cnts = np.zeros(datasets["train"]["y"].shape[1])
+        for model_idx in range(ARGS.num_models):
+        #for model_idx in range(1):
+            pred_name = "./preds/" + model_name + "-{}".format(model_idx) + "_batch-idx-{}".format(i)
+            sub_start = time.time()
+            with open(pred_name, "rb") as F:
+                preds = pickle.load(F)
+            preds, indices = preds
+            print("preds cost:", time.time()-sub_start, flush=True)
+            sub_start = time.time()
+            preds = preds.toarray(order='F')
+            total_preds[:, indices] += preds
+            total_cnts[indices] += 1
+            print("add preds cost:", time.time()-sub_start, flush=True)
+
+        sub_start = time.time()
+        target = datasets["test"]["y"][i * batch_size : (i + 1) * batch_size].toarray()
+        total_preds /= total_cnts+1e-16
+        print("cal total preds cost:", time.time()-sub_start, flush=True)
+        sub_start = time.time()
+        metrics.update(total_preds, target)
+        print("cal metrics cost:", time.time()-sub_start, flush=True)
+        with open(model_name + "-metrics-batch-idx-{}".format(i), 'wb') as F:
+            pickle.dump(metrics, F)
+        print("cost:", time.time()-start, flush=True)
+
+    return metrics.compute()
+
 
 def metrics_in_batches(model_name, batch_size):
     num_instances = datasets["test"]["x"].shape[0]
@@ -106,6 +144,38 @@ def metrics_in_batches(model_name, batch_size):
 
     return metrics.compute()
 
+    # # for amazon-670k full only
+    # submodel_name = "./models/" + model_name + "-{}".format(0)
+    # sub_start = time.time()
+    # 
+    # with open(submodel_name, "rb") as F:
+    #     tmp = pickle.load(F)
+    # tmp, indices = tmp
+    # print("model loaded:", time.time()-sub_start, flush=True)
+    # for i in range(num_batches):
+    #     print("process batches id:", i, flush=True)
+    #     start = time.time()
+    #     tmp_data = datasets["test"]["x"][i * batch_size : (i + 1) * batch_size]
+    #     total_preds = np.zeros([tmp_data.shape[0], datasets["train"]["y"].shape[1]], order='F')
+    #     total_cnts = np.zeros(datasets["train"]["y"].shape[1])
+    #         
+    #     sub_start = time.time()
+    #     preds = tmp.predict_values(tmp_data, beam_width=ARGS.beam_width)
+    #     print("preds cost:", time.time()-sub_start, flush=True)
+    #     sub_start = time.time()
+    #     preds = preds.toarray(order='F')
+    #     total_preds[:, indices] += preds
+    #     total_cnts[indices] += 1
+    #     print("add preds cost:", time.time()-sub_start, flush=True)
+
+    #     target = datasets["test"]["y"][i * batch_size : (i + 1) * batch_size].toarray()
+    #     total_preds /= total_cnts+1e-16
+    #     metrics.update(total_preds, target)
+    #     print("cost:", time.time()-start, flush=True)
+
+    # return metrics.compute()
+
+
 model_name = "Rand-label-Forest_{data}_seed={seed}_K={K}_sample-rate={sample_rate}.model".format(
         seed = ARGS.seed,
         K = ARGS.K,
@@ -113,9 +183,11 @@ model_name = "Rand-label-Forest_{data}_seed={seed}_K={K}_sample-rate={sample_rat
         data = os.path.basename(ARGS.datapath)
         )
 
-#metrics = metrics_in_batches(model_name, 20000)
+# metrics = metrics_in_batches(model_name, 10000)
+# for amazon-3m
+metrics = metrics_in_batches_without_pred(model_name, 10000)
 #metrics = metrics_in_batches(model_name, 1000)
-predict_in_batches(model_name, 10000, ARGS.idx)
+#predict_in_batches(model_name, 10000, ARGS.idx)
 
 print("mean in subsampled labels:", metrics)
 
