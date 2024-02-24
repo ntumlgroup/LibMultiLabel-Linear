57aee7028f4d7e3e4b796c5781f888f3a6701fa4
Sheng-Wei Chen <will945945945@gmail.com>
2024-02-24 04:07:22 +0000

    upgrade metrics

--- libmultilabel/linear/metrics.py
+++ libmultilabel/linear/metrics.py
@@ -7,32 +7,49 @@ import numpy as np
 __all__ = ["get_metrics", "compute_metrics", "tabulate_metrics", "MetricCollection"]
 
 
-def _DCG(preds: np.ndarray, target: np.ndarray, k: int = 5) -> np.ndarray:
-    """Compute the discounted cumulative gains (DCG)."""
-    # Self-implemented dcg is used here. scikit-learn's implementation has
-    # an average time complexity O(NlogN) as it directly applies quicksort
-    # to preds regardless of k. Here the average time complexity is reduced to
-    # O(N + klogk) by first partitioning off the k largest elements and then
-    # applying quicksort to the subarray.
-    row_idx = np.arange(preds.shape[0])[:, np.newaxis]
-    preds_unsorted_idx = np.argpartition(preds, -k)[:, -k:]
-    preds_sorted_idx = preds_unsorted_idx[row_idx, np.argsort(-preds[row_idx, preds_unsorted_idx])]
+def _argsort_top_k(preds: np.ndarray, top_k: int) -> np.ndarray:
+    """Sorts the top k indices in O(n + k log k) time.
+    The sorting order is ascending to be consistent with np.sort.
+    This means the last element is the largest, the first element is the kth largest.
+    """
+    top_k_idx = np.argpartition(preds, -top_k)[:, -top_k:]
+    argsort_top_k = np.argsort(np.take_along_axis(preds, top_k_idx, axis=-1))
+    return np.take_along_axis(top_k_idx, argsort_top_k, axis=-1)
 
-    # target sorted by the top k preds in non-increasing order
-    gains = target[row_idx, preds_sorted_idx]
 
-    # the discount factor
-    discount = 1 / (np.log2(np.arange(gains.shape[1]) + 2))
-
+def _dcg_argsort(argsort_preds: np.ndarray, target: np.ndarray, top_k: int) -> np.ndarray:
+    """Computes DCG@k with a sorted preds array and a target array."""
+    top_k_idx = argsort_preds[:, -top_k:][:, ::-1]
+    gains = np.take_along_axis(target, top_k_idx, axis=-1)
+    discount = 1 / (np.log2(np.arange(top_k) + 2))
     # get the sum product over the last axis of gains and discount
-    dcg = gains.dot(discount)
-    return dcg
+    dcgs = gains.dot(discount)
+    return dcgs
 
 
-class NDCG:
+def _idcg(target: np.ndarray, top_k: int) -> np.ndarray:
+    """Computes IDCG@k for a 0/1 target array. A 0/1 target is a special case that
+    doesn't require sorting. If IDCG is computed with DCG,
+    then target will need to be sorted, which incurs a large overhead.
+    """
+    num_relevant_labels = target.sum(axis=1, dtype="i")
+    discount = 1 / (np.log2(np.arange(top_k) + 2))
+    cum_discount = discount.cumsum()
+    # NDCG for an instance with no relevant labels is defined to be 0.
+    # If an instance has no relevant labels, its DCG will be 0,
+    # thus we can return any non-zero IDCG for that instance.
+    # Here we return the first element of cum_discount, which is 1.
+    idx = np.clip(num_relevant_labels - 1, 0, top_k - 1)
+    return cum_discount[idx]
+
+
+class NDCGAtK:
+    """Compute the normalized DCG@k (nDCG@k). Please refer to the `implementation document`
+    (https://www.csie.ntu.edu.tw/~cjlin/papers/libmultilabel/libmultilabel_implementation.pdf) for details.
+    """
+
     def __init__(self, top_k: int):
-        """Compute the normalized DCG@k (nDCG@k).
-
+        """
         Args:
             top_k: Consider only the top k elements for each query.
         """
@@ -44,18 +61,15 @@ class NDCG:
 
     def update(self, preds: np.ndarray, target: np.ndarray):
         assert preds.shape == target.shape  # (batch_size, num_classes)
+        return self.update_argsort(_argsort_top_k(preds, self.top_k), target)
 
-        # DCG
-        dcgs = _DCG(preds, target, self.top_k)
-        # ideal DCG
-        idcgs = _DCG(target, target, self.top_k)
-        # normalized DCG
-        ndcg_score = dcgs / idcgs
-
-        # deal with instances with all 0 labels and add up the results
+    def update_argsort(self, argsort_preds: np.ndarray, target: np.ndarray):
+        dcg = _dcg_argsort(argsort_preds, target, self.top_k)
+        idcg = _idcg(target, self.top_k)
+        ndcg_score = dcg / idcg
+        # by convention, ndcg is 0 for zero label instances
         self.score += np.nan_to_num(ndcg_score, nan=0.0).sum()
-        # remember the total number of instances
-        self.num_sample += preds.shape[0]
+        self.num_sample += argsort_preds.shape[0]
 
     def compute(self) -> float:
         return self.score / self.num_sample
@@ -65,10 +79,13 @@ class NDCG:
         self.num_sample = 0
 
 
-class RPrecision:
+class RPrecisionAtK:
+    """Compute the R-Precision@K. Please refer to the `implementation document`
+    (https://www.csie.ntu.edu.tw/~cjlin/papers/libmultilabel/libmultilabel_implementation.pdf) for details.
+    """
+
     def __init__(self, top_k: int):
-        """Compute the R-Precision@K.
-
+        """
         Args:
             top_k: Consider only the top k elements for each query.
         """
@@ -80,10 +97,14 @@ class RPrecision:
 
     def update(self, preds: np.ndarray, target: np.ndarray):
         assert preds.shape == target.shape  # (batch_size, num_classes)
-        top_k_idx = np.argpartition(preds, -self.top_k)[:, -self.top_k :]
-        num_relevant = np.take_along_axis(target, top_k_idx, axis=-1).sum(axis=-1)  # (batch_size, top_k)
+        return self.update_argsort(np.argpartition(preds, -self.top_k), target)
+
+    def update_argsort(self, argsort_preds: np.ndarray, target: np.ndarray):
+        top_k_idx = argsort_preds[:, -self.top_k :]
+        num_relevant = np.take_along_axis(target, top_k_idx, axis=-1).sum(axis=-1)  # (batch_size, )
+        # by convention, rprecision is 0 for zero label instances
         self.score += np.nan_to_num(num_relevant / np.minimum(self.top_k, target.sum(axis=-1)), nan=0.0).sum()
-        self.num_sample += preds.shape[0]
+        self.num_sample += argsort_preds.shape[0]
 
     def compute(self) -> float:
         return self.score / self.num_sample
@@ -93,18 +114,16 @@ class RPrecision:
         self.num_sample = 0
 
 
-class Precision:
-    def __init__(self, num_classes: int, average: str, top_k: int):
-        """Compute the Precision@K.
+class PrecisionAtK:
+    """Compute the Precision@K. Please refer to the `implementation document`
+    (https://www.csie.ntu.edu.tw/~cjlin/papers/libmultilabel/libmultilabel_implementation.pdf) for details.
+    """
 
+    def __init__(self, top_k: int):
+        """
         Args:
-            num_classes: The number of classes.
-            average: Define the reduction that is applied over labels. Currently only "samples" is supported.
             top_k: Consider only the top k elements for each query.
         """
-        if average != "samples":
-            raise ValueError("unsupported average")
-
         _check_top_k(top_k)
 
         self.top_k = top_k
@@ -113,10 +132,48 @@ class Precision:
 
     def update(self, preds: np.ndarray, target: np.ndarray):
         assert preds.shape == target.shape  # (batch_size, num_classes)
-        top_k_idx = np.argpartition(preds, -self.top_k)[:, -self.top_k :]
+        return self.update_argsort(np.argpartition(preds, -self.top_k), target)
+
+    def update_argsort(self, argsort_preds: np.ndarray, target: np.ndarray):
+        top_k_idx = argsort_preds[:, -self.top_k :]
         num_relevant = np.take_along_axis(target, top_k_idx, -1).sum()
         self.score += num_relevant / self.top_k
-        self.num_sample += preds.shape[0]
+        self.num_sample += argsort_preds.shape[0]
+
+    def compute(self) -> float:
+        return self.score / self.num_sample
+
+    def reset(self):
+        self.score = 0
+        self.num_sample = 0
+
+
+class RecallAtK:
+    """Compute the Recall@K. Please refer to the `implementation document`
+    (https://www.csie.ntu.edu.tw/~cjlin/papers/libmultilabel/libmultilabel_implementation.pdf) for details.
+    """
+
+    def __init__(self, top_k: int):
+        """
+        Args:
+            top_k: Consider only the top k elements for each query.
+        """
+        _check_top_k(top_k)
+
+        self.top_k = top_k
+        self.score = 0
+        self.num_sample = 0
+
+    def update(self, preds: np.ndarray, target: np.ndarray):
+        assert preds.shape == target.shape  # (batch_size, num_classes)
+        return self.update_argsort(np.argpartition(preds, -self.top_k), target)
+
+    def update_argsort(self, argsort_preds: np.ndarray, target: np.ndarray):
+        top_k_idx = argsort_preds[:, -self.top_k :]
+        num_relevant = np.take_along_axis(target, top_k_idx, -1).sum(axis=-1)
+        # by convention, recall is 0 for zero label instances
+        self.score += np.nan_to_num(num_relevant / target.sum(axis=-1), nan=0.0).sum()
+        self.num_sample += argsort_preds.shape[0]
 
     def compute(self) -> float:
         return self.score / self.num_sample
@@ -127,9 +184,12 @@ class Precision:
 
 
 class F1:
-    def __init__(self, num_classes: int, average: str, multiclass=False):
-        """Compute the F1 score.
+    """Compute the F1 score. Please refer to the `implementation document`
+    (https://www.csie.ntu.edu.tw/~cjlin/papers/libmultilabel/libmultilabel_implementation.pdf) for details.
+    """
 
+    def __init__(self, num_classes: int, average: str, multiclass=False):
+        """
         Args:
             num_classes: The number of labels.
             average: Define the reduction that is applied over labels. Should be one of "macro", "micro",
@@ -158,14 +218,15 @@ class F1:
     def compute(self) -> float:
         prev_settings = np.seterr("ignore")
 
+        # F1 is 0 for the cases where there are no positive instances
         if self.average == "macro":
             score = np.nansum(2 * self.tp / (2 * self.tp + self.fp + self.fn)) / self.num_classes
         elif self.average == "micro":
-            score = np.nan_to_num(2 * np.sum(self.tp) / np.sum(2 * self.tp + self.fp + self.fn))
+            score = np.nan_to_num(2 * np.sum(self.tp) / np.sum(2 * self.tp + self.fp + self.fn), nan=0.0)
         elif self.average == "another-macro":
             macro_prec = np.nansum(self.tp / (self.tp + self.fp)) / self.num_classes
             macro_recall = np.nansum(self.tp / (self.tp + self.fn)) / self.num_classes
-            score = np.nan_to_num(2 * macro_prec * macro_recall / (macro_prec + macro_recall))
+            score = np.nan_to_num(2 * macro_prec * macro_recall / (macro_prec + macro_recall), nan=0.0)
 
         np.seterr(**prev_settings)
         return score
@@ -183,6 +244,7 @@ class MetricCollection(dict):
 
     def __init__(self, metrics):
         self.metrics = metrics
+        self.max_k = max(getattr(metric, "top_k", 0) for metric in self.metrics.values())
 
     def update(self, preds: np.ndarray, target: np.ndarray):
         """Adds a batch of decision values and labels.
@@ -192,8 +254,18 @@ class MetricCollection(dict):
             target (np.ndarray): A 0/1 matrix of labels with dimensions number of instances * number of classes.
         """
         assert preds.shape == target.shape  # (batch_size, num_classes)
+
+        # The main bottleneck when computing metrics is sorting the top k indices.
+        # As an optimization, we sort only once and pass the sorted predictions to metrics that needs them.
+        # Top k ranking metrics only requires the sorted top k predictions, so we don't need to fully sort the predictions.
+        if self.max_k > 0:
+            argsort_preds = _argsort_top_k(preds, self.max_k)
+
         for metric in self.metrics.values():
-            metric.update(preds, target)
+            if hasattr(metric, "update_argsort"):
+                metric.update_argsort(argsort_preds, target)
+            else:
+                metric.update(preds, target)
 
     def compute(self) -> dict[str, float]:
         """Computes the metrics from the accumulated batches of decision values and labels.
@@ -229,11 +301,13 @@ def get_metrics(monitor_metrics: list[str], num_classes: int, multiclass: bool =
     metrics = {}
     for metric in monitor_metrics:
         if re.match("P@\d+", metric):
-            metrics[metric] = Precision(num_classes, average="samples", top_k=int(metric[2:]))
+            metrics[metric] = PrecisionAtK(top_k=int(metric[2:]))
+        elif re.match("R@\d+", metric):
+            metrics[metric] = RecallAtK(top_k=int(metric[2:]))
         elif re.match("RP@\d+", metric):
-            metrics[metric] = RPrecision(top_k=int(metric[3:]))
+            metrics[metric] = RPrecisionAtK(top_k=int(metric[3:]))
         elif re.match("NDCG@\d+", metric):
-            metrics[metric] = NDCG(top_k=int(metric[5:]))
+            metrics[metric] = NDCGAtK(top_k=int(metric[5:]))
         elif metric in {"Another-Macro-F1", "Macro-F1", "Micro-F1"}:
             metrics[metric] = F1(num_classes, average=metric[:-3].lower(), multiclass=multiclass)
         else:
