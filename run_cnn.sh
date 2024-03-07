c990532afc143915fd3a577b46b69f1e8575d379
Gordon119 <gordonsmart119@gmail.com>
2024-03-07 17:41:06 +0800

    update

--- run_cnn.sh
+++ run_cnn.sh
@@ -1,6 +1,6 @@
 for seed in 0 1109 172 309;do
-    for model in cnn_tune_no_tanh ;do
-        for task in MIMIC-50 SCOTUS;do
+    for model in cnn_tune_no_tanh cnn_tune ;do
+        for task in ECtHRB ECtHRA;do
             python search_params.py --config example_config/$task/$model.yml --seed $seed --no_checkpoint --no_retrain
         done
     done
