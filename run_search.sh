0c39f6bfa9fe939eb574e7b2d792fce1c465d78c
Gordon119 <gordonsmart119@gmail.com>
2024-03-22 12:07:29 +0000

    update

--- run_search.sh
+++ run_search.sh
@@ -1,8 +1,6 @@
 set -e
-for seed in 0 1109;do
-    for model in bilstm_tanhW_mlp_tune bilstm_tanhW_tune cnn_tanhW_mlp_tune cnn_tanhW_tune;do
+for seed in 0 1109 172 309;do
+    for model in bilstm_tanhW_mlp_tune bilstm_tanhW_tune bilstm_tanh_mlp_tune bilstm_tanh_tune bilstm_vanilla_mlp_tune bilstm_vanilla_tune cnn_tanhW_mlp_tune cnn_tanhW_tune cnn_tanh_mlp_tune cnn_tanh_tune cnn_vanilla_mlp_tune cnn_vanilla_tune;do
         python search.py --exps $seed/$model --config example_config/EUR-Lex/tune/$model.yml --template example_config/EUR-Lex/template.yml --seed $seed --no_checkpoint 
     done
 done
-# 1109 172 309
-# bilstm_tanhW_mlp_tune bilstm_tanhW_tune bilstm_tanh_mlp_tune bilstm_tanh_tune bilstm_vanilla_mlp_tune bilstm_vanilla_tune cnn_tanhW_mlp_tune cnn_tanhW_tune cnn_tanh_mlp_tune cnn_tanh_tune cnn_vanilla_mlp_tune cnn_vanilla_tune
