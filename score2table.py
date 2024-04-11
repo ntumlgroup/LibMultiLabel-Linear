c118388906e59f258f9a6c3c98f3d151bec6f25c
Gordon119 <gordonsmart119@gmail.com>
2024-04-12 05:54:07 +0800

    udpate

--- score2table.py
+++ score2table.py
@@ -2,45 +2,13 @@ import glob
 import sys, json
 import argparse
 import pandas as pd
-
-def make_markdown_table(array):
-
-    """ Input: Python list with rows of table as lists
-               First element as header. 
-        Output: String to put into a .md file 
-        
-    Ex Input: 
-        [["Name", "Age", "Height"],
-         ["Jake", 20, 5'10],
-         ["Mary", 21, 5'7]] 
-    """
-
-
-    markdown = "\n" + str("| ")
-
-    for e in array[0]:
-        to_add = " " + str(e) + str(" |")
-        markdown += to_add
-    markdown += "\n"
-
-    markdown += '|'
-    for i in range(len(array[0])):
-        markdown += str("-------------- | ")
-    markdown += "\n"
-
-    for entry in array[1:]:
-        markdown += str("| ")
-        for e in entry:
-            to_add = str(e) + str(" | ")
-            markdown += to_add
-        markdown += "\n"
-
-    return markdown + "\n"
+from pathlib import Path
 
 def make_csv_table(array):
     df = pd.DataFrame(array[1:], columns=array[0])
-    df_mean = df.groupby('model').mean().mul(100).round(2).reset_index()
-    df_std = df.groupby('model').std().mul(100).round(2).reset_index()
+    df_mean = df.groupby('model').mean().mul(100).applymap('{:2.2f}'.format).reset_index()
+
+    df_std = df.groupby('model').std().mul(100).applymap('{:.2f}'.format).reset_index()
     df = df_mean.merge(df_std, on="model", suffixes=["_avg", "_std"])
     return df
 
@@ -49,62 +17,119 @@ def main():
     parser.add_argument('-r', '--root', default="runs")
     parser.add_argument('-m', '--metrics',nargs="*", default=["Macro-F1", "Micro-F1", "P@5", "nDCG@5"])
     parser.add_argument('-t', '--tasks',nargs="*", default=["EUR-Lex", "MIMIC-50", "ECtHRA"])
-    parser.add_argument('-c', '--csv', action='store_true')
     parser.add_argument('-s', '--search', action='store_true')
+    parser.add_argument('-c', '--csv', action='store_true')
     parser.add_argument('-p', '--paper', action='store_true')
-    parser.add_argument('-d', '--des', type=str, default="trial_best_params")
-
     args = parser.parse_args()
-    json_files = sorted(glob.glob(f"{args.root}/*/{args.des}/logs.json")) if args.search else sorted(glob.glob(f"{args.root}/*/logs.json"))
+
+    json_files = sorted(glob.glob(f"{args.root}/*/trial_best_params/logs.json")) if args.search else sorted(glob.glob(f"{args.root}/*/logs.json"))
     tasks = dict()
+    undone = []
     for i in args.tasks:
         tasks[i] = [["model"]]
         tasks[i][0] += [i for i in args.metrics]
     for file in json_files:
         with open(file, 'r') as r:
             log = json.load(r)
-        if log["config"].get("custom_run_name", False):
-            # print(file)
-            task = "".join(log["config"]["result_dir"].split("_")[0])
-            task = task.replace("runs/", "")
-            model_name = "_".join(log["config"]["result_dir"].split("_")[1:-1])
-        else:
-            # breakpoint()
-            task = log["config"]["data_name"]
-            if args.des == "results":
-                model_name = "_".join(log["config"]["result_dir"].split("_")[1:-1])
-            else:
-                model_name = "_".join(log["config"]["run_name"].split("_")[1:-1])
-        if "test" not in log.keys():
-            print(file)
+        # check if the experiment is completed
+        if not log.get("test", False) or not log.get("config", False):
+            undone.append(file)
             continue
+        # check if the task is required.
+        task = log["config"]["data_name"]
+        if task not in args.tasks:
+            continue
+        # get model name (config name)
+        model_name = Path(log["config"]["config"]).stem
+        # get required metrics
         score = log["test"][0]
-        row = []
-        row.append(model_name)
-        for i in score.keys():
-            if i in args.metrics:
-                row.append(round(score[i], 4))
-        # print(task)
-        if task not in args.task:
-            continue
+        row = [model_name]
+        for key, value in score.items():
+            row.append(round(value, 4)) if key in args.metrics else None
         tasks[task].append(row)
-    
+    print(undone)
     if not args.paper:
         for task in tasks.keys():
             print(f"{task}:")
-            if args.csv:
-                print(make_csv_table(tasks[task]).to_csv(index=False, header=True))
-            else:
-                print(make_markdown_table(tasks[task]))    
+            print(make_csv_table(tasks[task]).to_csv(index=False, header=True))
     else:
         results = []
-        for task in tasks.keys():
-            print(task)
+        replace_dict = {
+            "bilstm_": "",
+            "cnn_": "",
+            "tanhW_": "\eqref{eq:tanhW}\_",
+            "tanh_":"\eqref{eq:cnn_lwan}\_",
+            "vanilla_":"\eqref{eq:vanilla-dot-product}\_",
+            "mlp_tune":"\eqref{eq:mlp}",
+            "_tune":"\eqref{eq:output_vanilla}"
+        }
+        tasks_list = list(tasks.keys())
+        for task in tasks_list:
             results.append(make_csv_table(tasks[task]))
-        for rows in zip(*[i.iterrows() for i in results]):
-            row_pair = []
-            for i in rows:
-                row_pair.append((" \pm ").join([str(i[1][j]) for j in range(1, len(i[1]))]))
-            print("& " + (" & ").join(row_pair))
+        task_p = r" & \bfseries ".join(tasks_list)
+        table = ""
+        table += r"& \bfseries " + task_p + r"\\" + "\n"
+        table += r"\bfseries Model & & Micro-F1 &  \\ \midrule BiLSTM \\"
+        for i in range(len(results[0][0:6])):
+            row = []
+            model_name = str(results[0].loc[i, "model"])
+            for key, val in replace_dict.items():
+                model_name = model_name.replace(key, val)
+            row.append(model_name)
+            for result in results:
+                mean = result.loc[i, f"{args.metrics[0]}_avg"]
+                std = result.loc[i, f"{args.metrics[0]}_std"]
+                text = f"{mean} $\pm$ {std}"
+                if mean == result[f"{args.metrics[0]}_avg"][0:6].max():
+                    row.append("\B{" + text + "}")
+                elif mean == result[f"{args.metrics[0]}_avg"][0:6].min():
+                    
+                    row.append(r"\underline{" + text + "}")
+                else:
+                    row.append(text)
+            table += " & ".join(row) + r"\\" + "\n"
+        table += r"\midrule" + "\n"
+        row = [r"\bfseries Max-min"]
+        for result in results:
+            max = result[f"{args.metrics[0]}_avg"][0:6].max()
+            min = result[f"{args.metrics[0]}_avg"][0:6].min()
+            val = float(max)-float(min)
+            val = round(val, 2)
+            row.append(str(val))
+        table += " & ".join(row) + "\n"
+        table += r"\midrule" + "\n"
+        table += r"CNN \\" + "\n"
+        for i in range(len(results[0][6:12])):
+            row = []
+            model_name = str(results[0].loc[i, "model"])
+            for key, val in replace_dict.items():
+                model_name = model_name.replace(key, val)
+            row.append(model_name)
+            for result in results:
+                mean = result.loc[i, f"{args.metrics[0]}_avg"]
+                std = result.loc[i, f"{args.metrics[0]}_std"]
+                text = f"{mean} $\pm$ {std}"
+                if mean == result[f"{args.metrics[0]}_avg"][6:12].max():
+                    row.append("\B{" + text + "}")
+                elif mean == result[f"{args.metrics[0]}_avg"][6:12].min():
+                    
+                    row.append(r"\underline{" + text + "}")
+                else:
+                    row.append(text)
+            table += " & ".join(row) + r"\\" + "\n"
+        table += r"\midrule" + "\n"
+        row = [r"\bfseries Max-min"]
+        for result in results:
+            max = result[f"{args.metrics[0]}_avg"][6:12].max()
+            min = result[f"{args.metrics[0]}_avg"][6:12].min()
+            val = float(max)-float(min)
+            val = round(val, 2)
+            row.append(str(val))
+        table += " & ".join(row) + "\n"
+        table += r"\bottomrule" + "\n"     
+
+        print(table)
+
+            
 if __name__ == "__main__":
     main()
